87bcb40f1787f6a829927e4dd7f34934a1799d19
amolg <amolg@datalogysoftware.com>
2024-02-13 22:03:57 +0530

    register fntionality

--- node-realtime-pizza/app/http/controllers/authController.js
+++ node-realtime-pizza/app/http/controllers/authController.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../../models/user.js');
 
 function authController(){
     return{
@@ -6,6 +9,43 @@ function authController(){
         },
         register(req, res){
             res.render("auth/register");
+        },
+        async postRegister(req, res){
+            const { name, email, password } = req.body;
+
+            /* Validate Fields */
+            if(!name || !email || !password){
+                req.flash('error', 'All Fields are required!');
+                req.flash('name', name);
+                req.flash('email', email);
+                return res.redirect('/register');
+            }
+
+            /* Check if email exist */
+            const existingUser = await User.findOne({ email });
+            if (existingUser) {
+                req.flash('error', 'Email already taken!');
+                req.flash('name', name);
+                req.flash('email', email);
+                return res.redirect('/register');
+            }
+            
+            /* Hash */
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            /* Create New user */
+            const user = new User({
+                name,
+                email,
+                password: hashedPassword
+            });
+
+            user.save().then(()=>{
+                return res.redirect('/');
+            }).catch(err=>{
+                req.flash('error', 'Something went wrong!');
+                return res.redirect('/register');
+            });
         }
     }
 }
--- node-realtime-pizza/routes/web.js
+++ node-realtime-pizza/routes/web.js
@@ -7,7 +7,9 @@ function initRoutes(app){
     app.get('/', homeController().index);
         
     app.get('/login', authController().login);
+
     app.get('/register', authController().register);
+    app.post('/register', authController().postRegister);
 
     app.get('/cart', cartController().index);
     app.post('/update-cart', cartController().update);
