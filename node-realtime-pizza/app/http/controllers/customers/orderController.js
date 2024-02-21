9fe470f113d24509a1e8c4ef1b0279b7e9e6a809
amolg <amolg@datalogysoftware.com>
2024-02-21 22:42:09 +0530

    remove flash message from cache on browser back button

--- node-realtime-pizza/app/http/controllers/customers/orderController.js
+++ node-realtime-pizza/app/http/controllers/customers/orderController.js
@@ -11,6 +11,9 @@ function orderController(){
                                     { sort: { 'createdAt': -1 } }
                                 );
 
+            /* Cache controlller */
+            res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+
             res.render('customers/orders', {orders:orders, moment:moment});
         },
         store(req, res){
